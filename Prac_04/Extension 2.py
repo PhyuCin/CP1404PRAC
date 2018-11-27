23f2936abedc300be5072fac68da0968d3cd5ab8
Aye Mya Phyu Cin <ayemyaphyucin@my.jcu.edu.au>
2018-11-27 15:30:29 +0800

    Complete extension 2 wk 4

--- Prac_04/Extension 2.py
+++ Prac_04/Extension 2.py
@@ -1,15 +1,17 @@
 numbers = []
-
-for i in range(0, 5):
-    num = int(input("Number: "))
+num = 0
+i = 1
+while not num < 0:
+    num = int(input("Number {}: ".format(i)))
+    i += 1
     numbers.append(num)
+del numbers[-1]
 print("The first number is", numbers[0])
 print("The last number is", numbers[-1])
 print("The smallest number is", min(numbers))
 print("The largest number is", max(numbers))
 
-average = sum(numbers)/len(numbers)
-
+average = sum(numbers)/(len(numbers))
+print(numbers)
 print("The average of the numbers is {:.2}".format(average))
-
 
