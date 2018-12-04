aaccc0996fa1d1afbc6d4d4a850d582521abcf4c
Aye Mya Phyu Cin <ayemyaphyucin@my.jcu.edu.au>
2018-12-04 14:27:46 +0800

    Complete do from scratch exercise prac 5

---
+++ Prac_05/do_from_scratch.py
@@ -0,0 +1,24 @@
+def main():
+    words_to_count = {}
+    string = input("Text: ")
+    words = string.split()
+    words_list = []
+
+    for i in range(len(words)):
+        count = words.count(words[i])
+        words_to_count[words[i]] = count
+
+    for word in words_to_count.keys():
+        words_list.append(word)
+    words_list.sort()
+    print(words_list)
+
+    for i in range(len(words_list)):
+        print("{} : {}".format(words_list[i], words_to_count[words_list[i]]))
+
+
+
+
+
+
+main()
