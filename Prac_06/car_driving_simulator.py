d857a91c3d5c76ded6b24b7a0868fd4f679e1594
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2018-12-11 22:26:38 +0800

    Ask for car name

---
+++ Prac_06/car_driving_simulator.py
@@ -0,0 +1,27 @@
+from Prac_06.cars import Car
+
+
+def main():
+    print("Let's drive!")
+    name = input("Enter your car name: ")
+    print(name)
+
+
+
+#     my_car = Car(180, "ferrari")
+#     my_car.drive(30)
+#
+#     print("fuel =", my_car.fuel)
+#     print("odo =", my_car.odometer)
+#     print(my_car)
+#     print()
+#
+#     limo = Car(100, "limo")
+#     limo.add_fuel(20)
+#     print("fuel =", limo.fuel)
+#     limo.drive(115)
+#     print("odo =", limo.odometer)
+#     print(limo)
+
+
+main()
