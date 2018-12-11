11c61a186a2252c558c909443d990a7ef794ca15
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2018-12-11 14:08:38 +0800

    Complete cars.py and used_cars.py walkthrough

---
+++ Prac_06/used_cars.py
@@ -0,0 +1,21 @@
+from Prac_06.cars import Car
+
+
+def main():
+    my_car = Car(180, "ferrari")
+    my_car.drive(30)
+
+    print("fuel =", my_car.fuel)
+    print("odo =", my_car.odometer)
+    print(my_car)
+    print()
+
+    limo = Car(100, "limo")
+    limo.add_fuel(20)
+    print("fuel =", limo.fuel)
+    limo.drive(115)
+    print("odo =", limo.odometer)
+    print(limo)
+
+
+main()
