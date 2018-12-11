6269ba97f033b6245867aed04aed6353dddb3d09
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2018-12-11 14:38:54 +0800

    complete programming languages exercise

---
+++ Prac_06/languages.py
@@ -0,0 +1,19 @@
+from Prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    programmes = [ruby, python, visual_basic]
+
+    for programme in programmes:
+        if programme.is_dynamic() is True:
+            print(programme)
+
+
+
+main()
+
+
