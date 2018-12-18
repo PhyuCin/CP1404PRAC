feb98ff960d29a9eb39cbee1f260e7781c7cab33
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2018-12-18 14:18:01 +0800

    Change colour of the output and the orientation of the display

--- Prac_07/KivyDemos-master/squaring.py
+++ Prac_07/KivyDemos-master/squaring.py
@@ -24,6 +24,7 @@ class SquareNumberApp(App):
     def handle_calculate(self, value):
         """ handle calculation (could be button press or other call), output result to label widget """
         result = value ** 2
+        result = float(result)
         self.root.ids.output_label.text = str(result)
 
 
