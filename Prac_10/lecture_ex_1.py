a0cf879b8ce21060ec5b8ca1dd1839155d410202
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2019-01-07 22:17:24 +0800

    add exercises

--- Prac_10/lecture_ex_1.py
+++ Prac_10/lecture_ex_1.py
@@ -1,12 +1,15 @@
-def fibonacci(n):
-    n1 = 0
-    n2 = 1
-    for i in range(0, n):
-        print(n1, end=',')
-        n3 = n1+n2
-        n1 = n2
-        n2 = n3
-    print()
+def rec_fun(n):
+    if n > 0:
+        return rec_fun(n - 1) + n * 2
+    return 0
 
 
-fibonacci(10)
+print(rec_fun(4))
+
+print(" ".join([str(i * 2) for i in range(1, 16, 3) if i % 2 == 0]))
+
+
+values = (2, 4, 6)
+for x, y in enumerate(values):
+    print("{0}:{1:.0f}".format(x, y), end=" ")
+
