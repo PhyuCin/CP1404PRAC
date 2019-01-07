b10525f32f83ebbac96edcad08b97c5192c860b6
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2019-01-07 22:16:00 +0800

    add name labels one by one

--- Prac_07/names.py
+++ Prac_07/names.py
@@ -1,27 +1,26 @@
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.core.window import Window
+from kivy.uix.label import Label
 
 
-class Milestokm(App):
-    """Display names"""
+class NamesApp(App):
     def __init__(self, **kwargs):
         """Construct main app."""
         super().__init__(**kwargs)
-        self.names = ["Bob Brown", "Cat Cyra", "Oren Ochre", "James Adams", "Suzy Bae"]
+        self.names = ["James", "Lauran", "Kerry", "Bruce"]
 
     def build(self):
-        """ build the Kivy app from the kv file """
-        Window.size = (500, 300)
+        """ build Kivy app from the kv file """
         self.title = "Names"
         self.root = Builder.load_file('names.kv')
+        self.display_name_labels()
         return self.root
 
-    def display_names(self):
-        """Create buttons from dictionary entries and add them to the GUI."""
+    def display_name_labels(self):
+        """Create labels from names and add them to the GUI."""
         for name in self.names:
-            self.root.ids.display_name.text = name
-            temp_label = Label(text=name, id=name)
-            temp_label.bind(on_release=self.press_entry)
-            # add the button to the "entries_box" layout widget
-            self.root.ids.entries_box.add_widget(temp_button)
+            # create a label for each name
+            self.root.add_widget(Label(text=name))
+
+
+NamesApp().run()
