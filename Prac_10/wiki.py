5a7b0a48aa4c1dca39365add44200dd0e0345b64
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2019-01-17 14:12:24 +0800

    print title, summary and url when given a page title

--- Prac_10/wiki.py
+++ Prac_10/wiki.py
@@ -9,7 +9,18 @@ def main():
         if search_phrase == "":
             continue
         elif wikipedia.suggest(search_phrase) is None:
-            print(wikipedia.summary(search_phrase))
+            try:
+                page = wikipedia.page(search_phrase)
+                title = page.title
+                summary = wikipedia.summary(search_phrase)
+                url = page.url
+                print(title)
+                print(summary)
+                print(url)
+            except wikipedia.exceptions.DisambiguationError as e:
+                print("Suggestions:\n" + ", ".join(e.options))
+            except wikipedia.exceptions.PageError:
+                print("The page", search_phrase, "does not exist")
         else:
             print("Suggestion:", wikipedia.suggest(search_phrase))
     print("Bye~")
